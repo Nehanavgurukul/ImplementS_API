a1c70d526b1be5f1f3d46e8a7cb0d48e979fec15
Nehanavgurukul <nehay19@navgurukul.org>
2021-06-30 23:40:38 +0530

    added APIs'

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.get("/get/All/Courses",(req,res) => {
     res.send(jsdata);
 });
 
+
+
 // create API for get course by id .
 app.get("/get/course/:id",(req,res) => {
     let jsondata = fs.readFileSync("data.json");
@@ -26,6 +28,34 @@ app.get("/get/course/:id",(req,res) => {
         }
     };
 });
+
+
+
+// create API for all exercise .
+app.get("/courses/full/exercise",(req,res) => {
+    let jsondata = fs.readFileSync("data.json");
+    let jsdata = JSON.parse(jsondata);
+    let exercise_array = []
+    for(i of jsdata){
+        exercise_array.push(i.exercise)
+    }
+    res.send(exercise_array)
+})
+
+
+
+//create API for course's exercise by id .
+app.get("/course/exercise/:id",(req,res) => {
+    let jsondata = fs.readFileSync("data.json");
+    let jsdata = JSON.parse(jsondata);
+    let id = req.params.id
+    for(i of jsdata){
+        if(i.id == id){
+            res.send(i.exercise)
+        }
+    }
+})
+
 
 
 // create API for add new course .
@@ -45,6 +75,53 @@ app.post("/add/course",(req,res) => {
 
 
 
+// create API for updating exercise .
+app.post("/add/exercise/:id",(req,res) => {
+    let jsondata = fs.readFileSync("data.json");
+    let jsdata = JSON.parse(jsondata);
+    let id = req.params.id
+    for(i of jsdata){
+        if(i.id == id){
+            let exercise_array = i.exercise
+            let body_data = {
+                id : i.exercise.length + 1,
+                exerName : req.body.exerName,
+                sub_exercise : req.body.sub_exercise
+            }
+            exercise_array.push(body_data)
+        }
+    }
+    fs.writeFileSync("data.json",JSON.stringify(jsdata,null , 2));
+    console.log("new course added .")
+    res.send(jsdata)
+})
+
+
+
+
+
+// create API FOR update the course .
+app.put("/update/:id",(req,res) => {
+    let jsondata = fs.readFileSync("data.json");
+    let jsdata = JSON.parse(jsondata);
+    let id = req.params.id
+    for(i of jsdata){
+        if(i.id == id){
+            i.id = id
+            i.courseName = req.body.courseName
+            i.exercise = req.body.exercise
+        }
+    }
+    fs.writeFileSync("data.json",JSON.stringify(jsdata,null , 2));
+    console.log("course updated  .")
+    res.send(jsdata)
+
+})
+
+
+
 app.listen(MY_PORT,() => {
     console.log(`server is running on >>>>> ${MY_PORT}`)
 });
+
+
